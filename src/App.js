b8e57d379ecd0c1b067452cd4cdd0623460e2c8a
pikssa <puru.14.kaushik@gmail.com>
2022-09-06 12:09:22 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react';
+import './Styles/App.css';
+import List from './List';
+import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom';
+
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
+
+
+function App() {
+
+  const [drinkList, setDrinkList] = useState([]);
+  const [input, setInput] = useState('');
+
+  const fetchUserData = () => {
+    fetch(url)
+    .then((data) => data.json())
+    .then((data) => {setDrinkList(data.drinks)})
+  }
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
+  }
+
+  useEffect(() => fetchUserData());
+
+  return (
+    <div className="App">
+      <Router>
+        <div className = "header">
+          <Link to = "/" className = "link">Home</Link>
+          <Link to = "/about" className = "link">About</Link>
+        </div>    
+      
+        <Switch>
+          <Route exact path = "/">
+            <div id = "search-bar-container">
+              <input type = "text" placeholder = "Search" className = "search-field" id = "search-bar" value = {input} onChange = {handleInputChange}></input>
+              <button id = "search-button"><i className = "fas fa-search fa-2x"></i></button>
+            </div>
+            <List drinkList = {drinkList} input = {input}/>
+          </Route>
+          <Route path = "/about">
+            <article id = "about-container">
+              <h1>About Page</h1>
+              <p>Lorem Ipsum</p>
+            </article>
+          </Route>
+        </Switch>
+      </Router>    
+    </div>
+  );
+}
+
+export default App;
